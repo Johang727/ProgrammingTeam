546338aa421a1725c4e48b907183bb700529064b
Johang727 <johanghernandez@hotmail.com>
2024-10-28 13:25:32 -0500

    added meysenburger's solution to the new alphabet

--- testttdict.py
+++ testttdict.py
@@ -4,17 +4,4 @@ adict = {'a':'@','b':'8','c':'(','d':'|)','e':'3','f':'#',
          'p':'|D','q':'(,)','r':'|Z','s':'$','t':"']['",
          'u':'|_|','v':'\/','w':'\/\/','x':'}{','y':'`/','z':'2'}
 
-''' TJ's and my answer
-theLine = input().lower()
-
-betterLine = ''
-for ch in theLine:
-    if ch in adict:
-        betterLine += adict[ch]
-    else:
-        betterLine += ch
-
-print(betterLine)
-'''
-
-print(''.join([adict.get(c,c) for c in input().lower()])) # meysenburger output
+print(adict['w'])
