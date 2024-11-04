ce03e5c4119555a33f06cce656fb0b0ad06c78ac
Johang727 <johanghernandez@hotmail.com>
2024-11-04 12:33:22 -0600

    base code works
    Co-Authored-By: BRRRARRGH <thure.w.johnson@gmail.com>

--- Python/t9spelling/t9spelling.py
+++ Python/t9spelling/t9spelling.py
@@ -1,6 +1,16 @@
 numLines = int(input())
 
-ourDict = {}
+t9 = {'a':'2','b':'22','c':'222'}
 
 for i in range(numLines):
     oldWord = input()
+    newWord = ''
+    for i in range(len(oldWord)):
+        if i == 0: # first letter
+            newWord += t9.get(oldWord[0])
+        else: # the rest of the letters
+            currentNum = t9.get(oldWord[i])[:1]
+            if currentNum == lastNum:
+                newWord += ' ' + t9.get(oldWord[i])
+        lastNum = t9.get(oldWord[i])[:1]
+    print(newWord)
