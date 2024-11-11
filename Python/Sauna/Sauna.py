8c8e24bc21a79eb2d51dee22606122a59ed0a7a8
Johang727 <johanghernandez@hotmail.com>
2024-11-11 12:47:52 -0600

    we're very good at coding :100: :100:
    Co-Authored-By: BRRRARRGH <thure.w.johnson@gmail.com>

--- Python/Sauna/Sauna.py
+++ Python/Sauna/Sauna.py
@@ -1,9 +1,12 @@
+# Created by TJ and Johang
+""" Attempt 1 - Too Slow
 goodDict = {}
 ins = int(input())
 for i in range(ins):
     newIn = input().split()
     for j in range(int(newIn[0]), int(newIn[1])):
         goodDict[j] = goodDict.get(j, 0) + 1
+
 goodVal = []
 
 for i in goodDict:
@@ -16,3 +19,27 @@ if len(goodVal) == 0:
     print("bad news")
 else:
     print(f"{ins} {goodVal[0]}")
+"""
+
+""" Attempt 2 - Wrong Answer #4
+inp = int(input())
+smallList = []
+bigList = []
+for i in range(inp):
+    newIn = input().split()
+    smallList.append(int(newIn[0]))
+    bigList.append(int(newIn[1]))
+
+smallList.sort()
+smallList.reverse()
+bigList.sort()
+
+#print(smallList)
+
+valueW = bigList[0] - smallList[0] + 1
+
+if valueW > 0:
+    print(f"{inp} {smallList[0]}")
+else:
+    print("bad news")
+"""
