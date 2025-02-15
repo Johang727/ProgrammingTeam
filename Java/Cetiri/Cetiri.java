445e62f5778e03165e90069def40a07fbfa6b896
Johang727 <johanghernandez@hotmail.com>
2025-02-15 16:24:43 -0600

    added the sort problem
    currently unfinished

---
+++ Java/Cetiri/Cetiri.java
@@ -0,0 +1,9 @@
+
+
+public class Cetiri {
+    public static void main(String[] args) {
+        int[] numIn = new int[3];
+
+        
+    }
+}
---
+++ Python/sort/sort.py
@@ -0,0 +1,57 @@
+"""
+First line of input contains 2 ints; len of message + number
+
+2nd line contains N positive integers, smaller or equal to C; the message itself
+
+Input EX:
+
+N C
+
+X Y Z A B
+
+more frequent appears before less frequent
+
+if numbers are the same frequency, they appear in order of message
+"""
+
+inp = input().split()
+
+"""n = int(inp[0]) # length of message
+c = int(inp[1]) # max value"""
+
+msg = [int(x) for x in input().split()]
+
+"""print(n)
+print(c)"""
+print(msg)
+
+"""msg_sort = msg[:] # we keep the original to maintain indexes
+
+msg_sort.sort(reverse=True)
+
+print(msg_sort)"""
+
+out_list = []
+
+counted = []
+
+for i in range(len(msg)):
+    if i != 0:
+        if msg[i] not in counted:
+            # check count of previous to check where to place
+            count1 = msg.count(msg[i])
+            count0 = msg.count(msg[i-1]
+            if count1 > count0:
+                # is greater, put left of i-1
+            elif count1 < count0:
+                # is less, put right of i-1
+            else: # is equal
+                # determine based on msg list where put. 
+
+    else:
+        for j in range(msg.count(msg[i])):
+            out_list.append(msg[i])
+            print(out_list)
+        counted.append(msg[i])
+
+
