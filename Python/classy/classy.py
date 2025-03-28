4367ceb40dd36245616dcbe996211350477887ee
Johang727 <johanghernandez@hotmail.com>
2025-03-28 13:13:42 -0500

    renammed the folders for better organization

--- Python/classy/classy.py
+++ Python/classy/classy.py
@@ -1,29 +1,16 @@
-'''input
-
-1
-5
-mom: upper-upper-lower-middle class
-dad: middle-middle-middle-lower-middle class
-queenelizabeth: upper-upper-upper class
-chair: lower-lower class
-unclebob: middle-middle-lower-middle class
-
-'''
-
 classNums = {"upper":1, "middle":2, "lower":3}
 
 
 numCases = int(input())
 
 for i in range(numCases):
-    peopleList = []
+    people = []
     numPeople = int(input())
     for j in range(numPeople):
-        inp = input().split(); #print(inp)
-        classes = inp[1].split('-'); #print(classes)
+        inp = input().split()
+        classes = inp[1].split('-')
         name = inp[0][:-1]
-        classesButBetter = [classNums[x] for x in classes]; #print(classesButBetter)
-        #peopleDict[name] = classesButBetter
+        classesButBetter = [classNums[x] for x in classes]
         
         while len(classesButBetter) < 10:
             classesButBetter.insert(0, 2)
@@ -31,24 +18,9 @@ for i in range(numCases):
 
         stringyList = ''.join(str(classesButBetter))
 
-        peopleList.append((stringyList, name))
+        people.append((stringyList, name))
 
-
-        
-    peopleList.sort()
-    for item in peopleList:
+    people.sort()
+    for item in people:
         print(item[1])
     print("==============================")
-
-
-#print(peopleDict)
-#print(peopleList)
-"""for item in peopleList:
-    print(item)
-
-peopleList.sort()
-print("\n after sort \n")
-for item in peopleList:
-    print(item)
-#print(maxVal)"""
-
