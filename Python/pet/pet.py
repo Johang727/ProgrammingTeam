fe59286cc2b625f96f3e22fa16427583f4bb4909
Johang727 <johanghernandez@hotmail.com>
2025-03-28 13:47:05 -0500

    finished pet, actual super easy

---
+++ Python/pet/pet.py
@@ -0,0 +1,34 @@
+import sys
+
+"""IN_1
+5 4 4 5
+5 4 4 4
+5 5 4 4
+5 5 5 4
+4 4 4 5
+
+Expected: 4 19
+"""
+# Determine who is the winner and how many points they got
+
+
+
+scores: list[int] = [int(x[:-1].replace(" ","")) for x in sys.stdin.readlines()]
+
+
+#print(scores)
+
+
+winner: int = -1
+winnerScore: int = -1
+
+for team in range(len(scores)):
+    total: int = 0
+    for score in str(scores[team]):
+        total += int(score)
+    
+    if total > winnerScore:
+        winnerScore = total
+        winner = team+1
+
+print(f"{winner} {winnerScore}")
